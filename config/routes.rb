353e69ab275b87c6314a70b13f4a2bb11bf4a70c
Mariana Furriel <marianafurrielss@gmail.com>
2024-07-22 14:19:51 -0300

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'about', to: 'pages#about' #get request to pages controller, about action
 #  get "up" => "rails/health#show", as: :rails_health_check
 
 end
