b6eafacfa555f81bd3d98816319f09958f8e414d
Mariana Furriel <marianafurrielss@gmail.com>
2024-07-22 15:06:12 -0300

    change folder structure

--- alpha_blog/config/routes.rb
+++ alpha_blog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about' #get request to pages controller, about action
 #  get "up" => "rails/health#show", as: :rails_health_check
