446f12c024d707774230110f9377cb8add492fcf
Mariana Furriel <marianafurrielss@gmail.com>
2024-08-19 15:23:35 -0300

    restartin

--- alpha-blog/db/migrate/20240722181345_creat_articles.rb
+++
@@ -1,4 +0,0 @@
-class CreatArticles < ActiveRecord::Migration[7.1]
-  def change
-  end
-end
--- demo/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :articles
-  root 'pages#home'
-  get 'about', to: 'pages#about' #get request to pages controller, about action
-#  get "up" => "rails/health#show", as: :rails_health_check
-
-end
