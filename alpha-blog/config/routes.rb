02c31c24f63eb58468018d0e79ab2abf6ccb7a0a
Mariana Furriel <marianafurrielss@gmail.com>
2024-08-25 08:15:33 -0300

    CRUD finalizado!

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles # all RESTful routes
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] # mostra todas as rotas, mesmo que linha de cima # all RESTful routes
 end
