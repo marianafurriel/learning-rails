67235ee58284d2740224c2a6a66837f1a8bb0e08
Mariana Furriel <marianafurrielss@gmail.com>
2024-08-26 17:47:55 -0300

    unit tests

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_19_185318) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_26_202237) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
