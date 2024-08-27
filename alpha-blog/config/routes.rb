956139c00994a50065aa92f8f66212a0b2274c38
Mariana Furriel <marianafurrielss@gmail.com>
2024-08-27 06:27:05 -0300

    more tests

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles # all RESTful routes
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] # mostra todas as rotas, mesmo que linha de cima # all RESTful routes
+  resources :categories, except: [:destroy]
 end
